49ee93777826095f155fcffd0fe5f407b86a2842
Alex R <areifert@users.noreply.github.com>
2021-12-21 09:48:55 -0700

    Initial Terraform commit

---
+++ example1.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-bucket"
+  acl = "private"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
