23d9a1c140ad4ae28bf2588f76a45c4f30318a05
Alex R <areifert@users.noreply.github.com>
2022-03-21 12:47:15 -0600

    Updating S3 resources to use newer syntax

--- terraform/example1/example1.tf
+++ terraform/example1/example1.tf
@@ -1,11 +1,19 @@
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-bucket"
+  tags = {
+    Name = "My Bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "my_bucket" {
+  bucket = aws_s3_bucket.my_bucket.id
   acl = "private"
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
+}
+
+resource "aws_s3_bucket_versioning" "my_bucket" {
+  bucket = aws_s3_bucket.my_bucket.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
