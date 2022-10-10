8fe22b146a2e4448b244039f9f5823010a8e8d65
areifert <areifert@users.noreply.github.com>
2022-10-10 16:54:30 -0600

    Adding ping CIDR range

--- SecurityGroups/bad_security_group.tf
+++ SecurityGroups/bad_security_group.tf
@@ -3,48 +3,49 @@ resource "aws_security_group" "linux_server_access" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description      = "Allow HTTP and HTTPS access"
-    from_port        = 80
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["10.0.0.0/8"]
+    description = "Allow HTTP and HTTPS access"
+    from_port   = 80
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]
   }
 
   ingress {
     description = "Allow pings"
-    from_port = "-1"
-    to_port = "-1"
-    protocol = "tcp"
+    from_port   = 8
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]
   }
 
   ingress {
-    description      = "Allow SSH access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "Allow SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description      = "Allow RDP access"
-    from_port        = 3389
-    to_port          = 3389
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "Allow RDP access"
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description      = "Allow MySQL access"
-    from_port        = 3306
-    to_port          = 3306
-    protocol         = "tcp"
-    cidr_blocks      = ["10.32.25.0/24"]
+    description = "Allow MySQL access"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.32.25.0/24"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
