de86013cca6fe8d2ae389802924ef350e8c25541
areifert <areifert@users.noreply.github.com>
2022-10-12 08:50:35 -0600

    Swapping handler1 and handler2

--- LambdaHandlers/handler1.py
+++ LambdaHandlers/handler1.py
@@ -1,22 +1,8 @@
 import json
-from mylib import process_record
+import os
 
-def handler(event, context):
-    status_code = 200
-    results = []
-    for record in event['Records']:
-        try:
-            data = json.loads(record['body'])
-            result = process_record(data)
-            results.append(result)
-        except Exception as e:
-            results.append({
-                'record_id': record['id'],
-                'error': str(e)
-            })
-            status_code = 500
-
-    return {
-        'statusCode': status_code,
-        'body': json.dumps(results)
-    }
+if __name__ == '__main__':
+    return json.dumps({
+        'data': os.environ.get('my_var'),
+        'status': 'OK'
+    })
--- LambdaHandlers/handler2.py
+++ LambdaHandlers/handler2.py
@@ -1,8 +1,22 @@
 import json
-import os
+from mylib import process_record
 
-if __name__ == '__main__':
-    return json.dumps({
-        'data': os.environ.get('my_var'),
-        'status': 'OK'
-    })
+def handler(event, context):
+    status_code = 200
+    results = []
+    for record in event['Records']:
+        try:
+            data = json.loads(record['body'])
+            result = process_record(data)
+            results.append(result)
+        except Exception as e:
+            results.append({
+                'record_id': record['id'],
+                'error': str(e)
+            })
+            status_code = 500
+
+    return {
+        'statusCode': status_code,
+        'body': json.dumps(results)
+    }
