ad930fe352ab1bbbf837d75cb160f9c770d72186
areifert <areifert@users.noreply.github.com>
2025-03-25 09:48:43 -0600

    Updating questions

--- LambdaHandlers/handler2.py
+++ LambdaHandlers/handler2.py
@@ -1,22 +1,32 @@
-import json
-from mylib import process_record
+import boto3
+from aws_lambda_powertools import Logger, Tracer
+from aws_lambda_powertools.utilities.batch import BatchProcessor, EventType, process_partial_response
+from aws_lambda_powertools.utilities.data_classes.sqs_event import SQSRecord
 
+logger = Logger()
+processor = BatchProcessor(event_type=EventType.SQS)
+tracer = Tracer()
+
+lambda_client = boto3.client('lambda')
+
+
+def record_handler(record: SQSRecord):
+    data: dict = record.json_body
+    logger.info(data)
+
+    lambda_client.invoke(
+        FunctionName=data['function_name'],
+        InvocationType='Event',
+        Payload=data['payload']
+    )
+
+
+@logger.inject_lambda_context
+@tracer.capture_lambda_handler
 def handler(event, context):
-    status_code = 200
-    results = []
-    for record in event['Records']:
-        try:
-            data = json.loads(record['body'])
-            result = process_record(data)
-            results.append(result)
-        except Exception as e:
-            results.append({
-                'record_id': record['id'],
-                'error': str(e)
-            })
-            status_code = 500
-
-    return {
-        'statusCode': status_code,
-        'body': json.dumps(results)
-    }
+    return process_partial_response(
+        event=event,
+        record_handler=record_handler,
+        processor=processor,
+        context=context
+    )
---
+++ blog-architecture.md
@@ -0,0 +1,11 @@
+# Designing a Serverless Blog in AWS
+You're tasked with designing a serverless web app in AWS, which hosts a blog. The website will:
+
+- Primarily host static content
+- Require user accounts/logins
+- Users will be able to add comments to blog posts
+- Users can also retrieve content via an API
+- Users can sign up for email notifications that are sent when a new blog post is published
+- Users can also sign up for daily email digests of new content
+
+Describe the architecture you would use to build this web app. Include the AWS services you would use and how they would interact with each other.
